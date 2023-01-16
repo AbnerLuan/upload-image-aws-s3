c2b0ffdd2d8e95c75a0b84fafdcc67787720afb5
AbnerLuan <abnerldsantos@gmail.com>
2023-01-15 23:08:46 -0300

    API de exemplo para carregar arquivos para a AWS S3

---
+++ src/main/java/com/luan/uploadimage/UploadImageAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.luan.uploadimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadImageAwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadImageAwsS3Application.class, args);
+	}
+
+}
